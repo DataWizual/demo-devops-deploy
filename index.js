8b7aa115071b76cb0b8ef65562c1f5550a81dc09
Eldor <DataWizual@users.noreply.github.com>
2025-10-25 21:49:49 +0500

    fix port for Railway

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from DevOps!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
